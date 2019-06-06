d2572d1ce808b4be7e111be4d477ae65508e6d96
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-06-06 16:51:47 -0700

    addition of publishing button to store blogs on firestore

--- js/login.js
+++ js/login.js
@@ -12,11 +12,18 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  var db = firebase.firestore();
+
+  var quill = new Quill('#editor', {
+    theme: 'snow'
+  });
+
   const emailField = document.getElementById('emailField')
   const passwordField = document.getElementById('passwordField')
   const loginButton = document.getElementById('login')
   const signupButton = document.getElementById('signup')
   const logoutButton = document.getElementById('logout')
+  const publishButton = document.getElementById('publish')
 
   loginButton.addEventListener('click', function(e) {
     const email = emailField.value
@@ -46,8 +53,31 @@
       logoutButton.classList.remove('d-none')
     } else {
       console.log('not logged in')
-        logoutButton.classList.add('d-none')
+      logoutButton.classList.add('d-none')
     }
+  })
+
+
+  publishButton.addEventListener('click', function(e){
+    // MARK: Code to save blog post
+    db.collection("blogPosts").add({
+        body: quill.getText(),
+        date: firebase.firestore.Timestamp.fromDate(new Date()),
+        title: "another post"
+      })
+      .then(function(docRef) {
+        db.collection("blogPosts").doc(docRef.id).collection("postComments").add({
+          body: ""
+        }).then(function(docRef) {
+          console.log("comments init: ", docRef.id)
+        }).catch(function(error) {
+          console.log('comments error: ', error)
+        })
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+        console.error("Error adding blog post: ", error);
+      });
   })
 
 
